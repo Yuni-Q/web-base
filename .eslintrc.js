f1b7876bbf1585a6a6a4f0bf10c9ea8b58dea474
Yuni-Q <carnotcycle2@naver.com>
2020-01-17 15:45:27 +0900

    기본 lint

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  "no-unexpected-multiline": "error",
+  "no-extra-semi": "error"
+};
--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
-import * as math from "./math";
-import "./style.css";
+// import * as math from "./math";
+// import "./style.css";
 
-console.log(math.sum(1, 2)); // 3
-console.log(123, process.env.NODE_ENV); // development
-console.log(456, process.env); // {}
+// console.log(math.sum(1, 2)); // 3
+// console.log(123, process.env.NODE_ENV); // development
+// console.log(456, process.env); // {}
 
-new Promise();
+// new Promise();
+
+console.log()(function() {});
